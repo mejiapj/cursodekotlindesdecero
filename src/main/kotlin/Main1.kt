53867ed37da36f104f959882caf25e056fab78db
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-09-30 19:39:07 -0500

    Clase 8/37 -> Variables en Kotlin

---
+++ src/main/kotlin/Main1.kt
@@ -0,0 +1,13 @@
+const val PI=3.1416
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    var dinero = 10
+    println(dinero)
+    dinero = 5
+    println(dinero)
+
+    val nombre = "Maria"
+    println(nombre)
+    println(PI)
+}
