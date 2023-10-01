318e3d2150ce8f8a3359f0c28a00e225f6035180
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-09-30 19:46:07 -0500

    Clase 9/37 -> Kotlin y sus tipos de variables

---
+++ src/main/kotlin/Main2.kt
@@ -0,0 +1,30 @@
+// const val PI=3.1416
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    var dinero = 10
+    println(dinero)
+    dinero = 5
+    println(dinero)
+
+    val nombre = "Maria"
+    println(nombre)
+    println(PI)
+
+    val boolean = true
+    val numeroLargo = 3L
+    val  double = 2.7182
+    val float= 1.1f
+
+    val primerValor = 20
+    val segundoValor = 10
+    val tercerValor = primerValor - segundoValor
+    println(tercerValor)
+
+    val apellidos = "Vetri"
+    val giuseppe = "Giuseppe"
+    val nombreCompleto = "Mi nombre es $giuseppe $apellidos"
+    println(nombreCompleto)
+
+}
