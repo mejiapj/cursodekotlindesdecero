e959f66d03fb7d19b800e7064a3b1c75eb3348eb
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-10-02 22:44:54 -0500

    Clase 16/37 -> Ciclos

---
+++ src/main/kotlin/Main6.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+val listaDeFrutas = listOf( "Manzana", "Pera", "Frambuesa", "Durazno" )
+for (fruta in listaDeFrutas) println("Hoy voy a comerme una fruta llamada $fruta")
+listaDeFrutas.forEach { fruta -> println("Hoy voy a comerme una fruta nueva llamada $fruta") }
+val caracteresDeFruta = listaDeFrutas.map { fruta-> fruta.length }
+    println(caracteresDeFruta)
+
+val listaFltrada = caracteresDeFruta.filter { largoDeFruta-> largoDeFruta>5 }
+    println(listaFltrada)
+
+
+}
