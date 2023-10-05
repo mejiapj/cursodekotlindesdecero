8f7bc6b8b92ad2f561adc91ab81e478b20309c13
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-10-04 21:45:18 -0500

    [200~

---
+++ src/main/kotlin/Main10.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    val numerosDeLoteria = listOf(11, 22, 43, 56, 78, 66)
+    val numerosSorted = numerosDeLoteria.sorted()
+    println(numerosSorted)
+
+    val numerosDeLoteriaDescendientes = numerosDeLoteria.sortedDescending()
+    println(numerosDeLoteriaDescendientes)
+    val ordenarPorMultiplos = numerosDeLoteria.sortedBy { numero -> numero < 50 }
+    println(ordenarPorMultiplos)
+
+    val numerosAletorios = numerosDeLoteria.shuffled()
+    println(numerosAletorios)
+
+    val numerosEnReversa = numerosDeLoteria.reversed()
+    println(numerosEnReversa)
+
+    val mensajesDeNumeros = numerosDeLoteria.map{ numero -> "Tu numero de loteria es $numero"}
+    println(mensajesDeNumeros)
+
+    val numerosFiltrados = numerosDeLoteria.filter { numero -> numero>50 }
+    println(numerosFiltrados)
+}
