a344fc2a0a931868108c3a3334377f4e5334d69e
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-10-07 12:52:49 -0500

    Clase 31/37 -> With

--- src/main/kotlin/Main18.kt
+++ src/main/kotlin/Main18.kt
@@ -1,4 +1,4 @@
-fun main(args: Array<String>) {
+run main(args: Array<String>) {
     val colores = listOf("Azul", "Amarillo", "Rojo")
     with(colores) {
         println("Nuestros colores son $this")
