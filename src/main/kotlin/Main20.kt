171440cd3abe1ed39864191b8219e24f2b0be765
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-10-07 12:58:53 -0500

    Clase 33/37 -> Apply

--- src/main/kotlin/Main20.kt
+++ src/main/kotlin/Main20.kt
@@ -1,5 +1,7 @@
+
+
 fun main(args: Array<String>) {
-    val moviles = mutableListOf("Google Pixel 2XL", "Google Pixel 4A", "Huawei Redmi 9", "Xaomi mi a3").apply {
+    val moviles = mutableListOf("Google pixel 2XL", "google Pixel 4A", "Huawei Redmi 9", "Xaomi mi a3").apply {
         removeIf { movil -> movil.contains("Google") }
     }
     println(moviles)
