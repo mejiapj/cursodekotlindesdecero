70c64744879ad17f07c73c62e9a3dc84ad2719f3
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-10-07 12:29:14 -0500

    Clase 30/37 -> Let

---
+++ src/main/kotlin/Main17.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+var nombre : String? = null
+    nombre?.let {
+        valor -> println("El nombre no es nulo, es $valor")
+    }
+    nombre = "Giuseppe"
+    nombre?.let {
+        valor -> println("El nombre no es nulo, es $valor")
+    }
+}
